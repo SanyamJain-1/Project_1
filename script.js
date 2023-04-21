7248bcfd9a75bda671d898ac87d34fe3fd78a441
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-22 01:34:26 +0530

    21/04

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstVariable = 10;
+let secondVariable = 'Hello World';
+let thirdVaribale = [firstVariable, secondVariable, [1,2,3,4], {
+    prop1: 'Javascript',
+    prop2: 'Playlist'
+}, 200];
+
+console.log(thirdVaribale);
