3dac27dd28fe8a127b4f5f9c21a696943a119de8
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-23 19:34:56 +0530

    23/04

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById("myButton").onclick = function(){
     secondSide = document.getElementById("secondSide").value;
     let thirdSide = Math.sqrt(Math.pow(firstSide,2)+Math.pow(secondSide,2));
     console.log('Legth of third side is' ,thirdSide);
+    document.getElementById("thirdSide").innerHTML = thirdSide;
 }
 
 
