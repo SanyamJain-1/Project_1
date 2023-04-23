5242d79f02d30a45c48677f21532e94fc694b73b
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-23 19:27:25 +0530

    23/04

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-document.getElementById("firstName").innerHTML = "Hello! Sanyam";
+let firstSide, secondSide;
+
+document.getElementById("myButton").onclick = function(){
+    firstSide = document.getElementById("firstSide").value;
+    secondSide = document.getElementById("secondSide").value;
+    let thirdSide = Math.sqrt(Math.pow(firstSide,2)+Math.pow(secondSide,2));
+    console.log('Legth of third side is' ,thirdSide);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
